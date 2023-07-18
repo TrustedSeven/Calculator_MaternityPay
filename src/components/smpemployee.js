4f52d6151ed83a7532f1e310e2e27843b84fd89d
TrustedSeven <castaneda.chistian0215@gmail.com>
2023-07-18 12:20:23 -0500

    salary input width fix

--- src/components/smpemployee.js
+++ src/components/smpemployee.js
@@ -184,7 +184,7 @@ export default function Smpemployee() {
               <input
                 type="number"
                 id="salary"
-                class="bg-gray-50 border w-[50%] border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                class="bg-gray-50 border w-[50%] max-w-sm border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 placeholder="£"
                 onChange={(event) => {
                   setSalary(event.target.value);
